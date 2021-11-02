38cb93660124a3df375064cdd8532ac67c712afa
Troy Hunt <github@troyhunt.com>
2021-11-03 08:03:16 +1000

    Removed close option for modal and auto-redirected to Coinhive blog post

--- coinhive.js
+++ coinhive.js
@@ -3,17 +3,14 @@
 let createModal = (modalContent) => {
   let modal = document.createElement('div'),
     modalStyle = document.createElement('style'),
-    modalCSS = '.js-modal{ position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: rgba(0, 0, 0, .8); width: 100%; height: 100%; z-index: 999999; } .js-modal-inner{ background-color: rgba(174, 145, 93, .9); position: relative; padding: 50px; font-size: 24px; max-width: 650px; top: 50%; left: 50%; transform: translate(-50%, -50%); color: #000; border-radius: 10px; font-family: Arial, Tahoma, Helvetica, FreeSans, sans-serif; line-height: normal; text-align: center; }  .js-modal-inner a { color: #000; text-decoration: underline; } .js-modal-close{ position: absolute; top: -10px; right: 0px; background-color: black; color: #eee; border-width: 0; font-size: 10px; height: 24px; width: 24px; border-radius: 100%; text-align: center; font-family: Arial; cursor: pointer;}',
-    modalClose = '<button class="js-modal-close" id="js_modal_close">X</button>',
+    modalCSS = '.js-modal{ position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: rgba(0, 0, 0, .8); width: 100%; height: 100%; z-index: 999999; } .js-modal-inner{ background-color: rgba(174, 145, 93, .9); position: relative; padding: 50px; font-size: 24px; max-width: 650px; top: 50%; left: 50%; transform: translate(-50%, -50%); color: #000; border-radius: 10px; font-family: Arial, Tahoma, Helvetica, FreeSans, sans-serif; line-height: normal; text-align: center; }  .js-modal-inner a { color: #000; text-decoration: underline; }',
     theBody = document.getElementsByTagName('body')[0],
     theHead = document.getElementsByTagName('head')[0];
 
   // Add content and attributes to the modal
   modal.setAttribute('class', 'js-modal');
-  modal.innerHTML = '<div class="js-modal-inner">' + modalContent + modalClose + '</div>';
+  modal.innerHTML = '<div class="js-modal-inner">' + modalContent + '</div>';
   theBody.appendChild(modal);
-
-  modalClose = document.querySelector('#js_modal_close');
 
   // Add the modal styles dynamically
   if(modalStyle.styleSheet){
@@ -22,18 +19,11 @@ let createModal = (modalContent) => {
     modalStyle.appendChild(document.createTextNode(modalCSS));
   }
   theHead.appendChild(modalStyle);
-
-  // Close the modal on button-click
-  if(modalClose) {
-    modalClose.addEventListener('click', function() {
-      modal.remove();
-      modalStyle.remove();
-    });
-  }
 }
 
 // Show it up when loading starts
 window.addEventListener('load', function() {
-  /* Remember to escape the characters to their respective valid HTML entities, for eg. ' will become \' */
-  createModal('This website attempted to run a cryptominer in your browser. <a href="https://www.troyhunt.com/i-now-own-the-coinhive-domain-heres-how-im-fighting-cryptojacking-and-doing-good-things-with-content-security-policies">Click here for more information</a>.');
+  let url = 'https://www.troyhunt.com/i-now-own-the-coinhive-domain-heres-how-im-fighting-cryptojacking-and-doing-good-things-with-content-security-policies';
+  createModal('This website attempted to run a cryptominer in your browser. <a href="' + url + '">Click here for more information</a>.');
+  setTimeout(function(){ location.href=url; }, 5000);
 });
